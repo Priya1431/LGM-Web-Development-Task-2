f86f2e281f9b38706186a42ababf0603a66ae1ba
Priya1431 <88383895+Priya1431@users.noreply.github.com>
2021-08-25 11:04:02 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,55 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from "./Components/Header";
+import UserCard from './Components/Usercard'
+import Loader from './Components/Loader'
+import Axios from 'axios';
 
+function App() {
+  const [isClicked, setIsClicked] = useState(0);
+  const [data, setData] = useState(null);
+
+
+  const [loading, setloading] = useState(false);
+
+  const getUsers = () => {
+    //loading ....
+    setloading(true)
+
+    Axios.get("https://reqres.in/api/users?page=1").then(
+      (response) => {
+        console.log("API DATA")
+        setData(response.data.data);
+
+        //Loading off
+        
+        setloading(false)
+      }
+    )
+  }
+  
+  
+  useEffect(() => {
+    isClicked && getUsers();
+  }, [isClicked])
+
+  
+  return (
+    <>
+    
+    <Header onSetIsClicked={setIsClicked}/>
+      <div className='row'>
+
+        {data&&data.map((val) => {
+          return <UserCard first_name={val.first_name} last_name={val.last_name} email={val.email} avatar={val.avatar}/>
+          
+        })}
+      </div>
+
+      <Loader load={loading} />
+    
+    </>
+  );
+}
+
+export default App
